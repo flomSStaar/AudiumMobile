f6589956e05cc22a75dc57c38952b1b0080f784b
ThomasWILHEM <thomaswilhempro@outlook.fr>
2022-04-06 16:09:30 -0400

    Ajout de l'ajout et de la suppression d'artiste par l'admin. Form de suppression encore à faire

--- app/src/main/java/uqac/dim/audium/ArtistListActivity.java
+++ app/src/main/java/uqac/dim/audium/ArtistListActivity.java
@@ -7,48 +7,69 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
 
 import uqac.dim.audium.model.entity.Artist;
-import uqac.dim.audium.model.entity.User;
 
 public class ArtistListActivity extends AppCompatActivity {
 
-    FirebaseFirestore db;
+    DatabaseReference database;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist_list);
         ArrayList<Artist> l = new ArrayList<>();
-        l.add(new Artist("Reginald","Kenneth Dwight","Elton John",75));
         ListView artistListView = ((ListView) findViewById(R.id.artistList));
-        db = FirebaseFirestore.getInstance();
-        db.collection("artists")
-                .get()
-                .addOnSuccessListener(queryDocumentSnapshots -> {
-                    for (int i = 0; i < queryDocumentSnapshots.getDocuments().size(); i++)
-                        l.add(queryDocumentSnapshots.getDocuments().get(i).toObject(Artist.class));
-                    artistListView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, l));
-                });
+        database = FirebaseDatabase.getInstance().getReference();
+        database.child("artists").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                l.clear();
+                for (DataSnapshot snap : snapshot.getChildren()) {
+                    Artist a = snap.getValue(Artist.class);
+                    l.add(a);
+                }
+                artistListView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, l));
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
         artistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
                 Intent intent = new Intent(ArtistListActivity.this, ArtistProfileActivity.class);
-                intent.putExtra("stageName", ((Artist)artistListView.getItemAtPosition(position)).getStageName());
+                intent.putExtra("id", ((Artist)artistListView.getItemAtPosition(position)).getId());
                 startActivity(intent);
             }
         });
-
-
     }
 
     public void addArtists(View view) {
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        db.getReference("ids/lastArtistId").get()
+                .addOnSuccessListener(dataSnapshot -> {
+                    Long lastArtistId = dataSnapshot.getValue(Long.class);
+                    if (lastArtistId != null) {
+                        Artist artist = new Artist("t", "t", "ouioui", 10, lastArtistId);
+                        db.getReference("artists/").child(String.valueOf(lastArtistId)).setValue(artist);
+                        db.getReference("ids/lastArtistId").setValue(++lastArtistId);
+                    }
+                });
     }
+
 }
--- app/src/main/java/uqac/dim/audium/UserListActivity.java
+++ app/src/main/java/uqac/dim/audium/UserListActivity.java
@@ -15,6 +15,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
@@ -23,11 +28,13 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import uqac.dim.audium.model.entity.Artist;
 import uqac.dim.audium.model.entity.User;
 
 public class UserListActivity extends AppCompatActivity {
 
-    FirebaseFirestore db;
+    DatabaseReference database;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,6 +42,7 @@ public class UserListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user_list);
         ArrayList<User> l = new ArrayList<>();
         ListView userListView = ((ListView) findViewById(R.id.userList));
+        /*
         db = FirebaseFirestore.getInstance();
         db.collection("users")
                 .get()
@@ -51,8 +59,33 @@ public class UserListActivity extends AppCompatActivity {
                 intent.putExtra("username", ((User)userListView.getItemAtPosition(position)).getUsername());
                 startActivity(intent);
             }
-        });
+        });*/
 
+        database = FirebaseDatabase.getInstance().getReference();
+        database.child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                l.clear();
+                for (DataSnapshot snap : snapshot.getChildren()) {
+                    User user = snap.getValue(User.class);
+                    l.add(user);
+                }
+                userListView.setAdapter(new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, l));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        userListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapter, View view, int position, long arg) {
+                Intent intent = new Intent(UserListActivity.this, UserProfileActivity.class);
+                intent.putExtra("username", ((User)userListView.getItemAtPosition(position)).getUsername());
+                startActivity(intent);
+            }
+        });
 
     }
 }
